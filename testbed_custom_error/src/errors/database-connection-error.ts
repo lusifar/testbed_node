ca4e26425ec58a4d483e4418ab1170f6a614188c
lusifar <lusifar@gmail.com>
2021-05-09 12:33:36 +0800

    add the project with testbed_custom_error

---
+++ testbed_custom_error/src/errors/database-connection-error.ts
@@ -0,0 +1,20 @@
+import { CustomError } from './custom-error';
+
+export class DatabaseConnectionError extends CustomError {
+    statusCode: number = 500;
+    reason: string = "Error for connecting to database";
+
+    constructor() {
+        super('Error for connecting to database');
+
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors(): { message: string; field?: string | undefined; }[] {
+        return [
+            {
+                message: this.reason,
+            }
+        ];
+    }
+}
---
+++ testbed_custom_error/src/errors/request-validation-error.ts
@@ -0,0 +1,23 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+export class RequestValidationError extends CustomError {
+    statusCode: number = 400;
+
+    constructor(public errors: ValidationError[]) {
+        super('Error for request validation');
+
+        Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+
+    serializeErrors(): { message: string; field?: string | undefined; }[] {
+        return this.errors.map((error) => {
+            return {
+                message: error.msg,
+                field: error.param,
+            }
+        });
+    
+    }
+    
+}
