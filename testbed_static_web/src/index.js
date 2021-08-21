ac712dc7f9b35bf64ee8368e1654ef8e45a175b6
rickychao <lusifar0910@gmail.com>
2021-08-21 20:46:49 +0800

    add the border-radius and box-shadow attributes in testbed_static_web project

--- testbed_static_web/src/index.js
+++ testbed_static_web/src/index.js
@@ -6,6 +6,9 @@ const color0 = '#92b2ed';
 const color1 = '#d3e0f8';
 const color2 = '#888888';
 const color3 = '#eeeeee';
+const padding = '5px 0';
+const shadow = '3px 3px 6px silver';
+const radius = '5px';
 
 const headers = ['Tool', 'Chamber', 'URL'];
 const bodies = [
@@ -17,26 +20,68 @@ const styleObject = {
   table: {
     border: `1px solid ${color0}`,
     backgroundColor: `${color0}`,
+    boxShadow: `${shadow}`,
+    borderRadius: `${radius}`,
   },
   thead: {
     th: {
-      padding: '10px 0px',
+      padding: `${padding}`,
       color: `${color1}`,
       backgroundColor: `${color0}`,
+    },
+    thTopLeft: {
+      padding: `${padding}`,
+      color: `${color1}`,
+      backgroundColor: `${color0}`,
+      borderTopLeftRadius: `${radius}`,
+    },
+    thTopRight: {
+      padding: `${padding}`,
+      color: `${color1}`,
+      backgroundColor: `${color0}`,
+      borderTopRightRadius: `${radius}`,
     },
   },
   tbody: {
     tdOdd: {
       textAlign: 'center',
-      padding: '10px 0px',
+      padding: `${padding}`,
       color: `${color0}`,
       backgroundColor: `${color1}`,
     },
+    tdOddBottomLeft: {
+      textAlign: 'center',
+      padding: `${padding}`,
+      color: `${color0}`,
+      backgroundColor: `${color1}`,
+      borderBottomLeftRadius: `${radius}`,
+    },
+    tdOddBottomRight: {
+      textAlign: 'center',
+      padding: `${padding}`,
+      color: `${color0}`,
+      backgroundColor: `${color1}`,
+      borderBottomRightRadius: `${radius}`,
+    },
     tdEven: {
       textAlign: 'center',
-      padding: '10px 0px',
+      padding: `${padding}`,
       color: `${color2}`,
       backgroundColor: `${color3}`,
+    },
+    tdEvenBottomLeft: {
+      textAlign: 'center',
+      padding: `${padding}`,
+      color: `${color2}`,
+      backgroundColor: `${color3}`,
+      borderBottomLeftRadius: `${radius}`,
+    },
+    tdEvenBottomRight: {
+      textAlign: 'center',
+      padding: `${padding}`,
+      color: `${color2}`,
+      backgroundColor: `${color3}`,
+      borderBottomRightRadius: `${radius}`,
     },
   },
 };
--- testbed_static_web/src/utilities/table.js
+++ testbed_static_web/src/utilities/table.js
@@ -2,10 +2,18 @@ const styleToCss = require('style-object-to-css-string');
 
 const genTHeader = (headers, styleObject) => {
   const thStyle = styleToCss(styleObject.th);
+  const thTopLeftStyle = styleToCss(styleObject.thTopLeft);
+  const thTopRightStyle = styleToCss(styleObject.thTopRight);
 
   let context = '';
-  headers.forEach((header) => {
-    context = context + `<th style="${thStyle}">${header}</th>`;
+  headers.forEach((header, index) => {
+    if (index === 0) {
+      context = context + `<th style="${thTopLeftStyle}">${header}</th>`;
+    } else if (index === headers.length - 1) {
+      context = context + `<th style="${thTopRightStyle}">${header}</th>`;
+    } else {
+      context = context + `<th style="${thStyle}">${header}</th>`;
+    }
   });
 
   return `<thead><tr>${context}</tr></thead>`;
@@ -13,13 +21,25 @@ const genTHeader = (headers, styleObject) => {
 
 const genTBody = (bodies, styleObject) => {
   const tdOddStyle = styleToCss(styleObject.tdOdd);
+  const tdOddBottomLeftStyle = styleToCss(styleObject.tdOddBottomLeft);
+  const tdOddBottomRightStyle = styleToCss(styleObject.tdOddBottomRight);
   const tdEvenStyle = styleToCss(styleObject.tdEven);
+  const tdEvenBottomLeftStyle = styleToCss(styleObject.tdEvenBottomLeft);
+  const tdEvenBottomRightStyle = styleToCss(styleObject.tdEvenBottomRight);
 
   let context = '';
-  bodies.forEach((items, index) => {
+  bodies.forEach((items, index0) => {
     context = context + `<tr>`;
-    items.forEach((item) => {
-      context = context + `<td style="${index % 2 == 0 ? tdEvenStyle : tdOddStyle}">${item}</td>`;
+    items.forEach((item, index1) => {
+      if (index0 === bodies.length - 1 && index1 === 0) {
+        context =
+          context + `<td style="${index0 % 2 == 0 ? tdEvenBottomLeftStyle : tdOddBottomLeftStyle}">${item}</td>`;
+      } else if (index0 === bodies.length - 1 && index1 === items.length - 1) {
+        context =
+          context + `<td style="${index0 % 2 == 0 ? tdEvenBottomRightStyle : tdOddBottomRightStyle}">${item}</td>`;
+      } else {
+        context = context + `<td style="${index0 % 2 == 0 ? tdEvenStyle : tdOddStyle}">${item}</td>`;
+      }
     });
     context = context + '</tr>';
   });
